321c2a2207837fb07de596703e382b1fc9cf3041
zhangfan <wingame7@sina.cn>
2021-10-12 14:49:48 +0800

    nkn配置tls改为true, 因为线上的https环境不能连非wss的节点

--- src/apollo/nknConfig.ts
+++ src/apollo/nknConfig.ts
@@ -5,7 +5,7 @@ import { MAX_MTU, NKN_SUB_CLIENT_COUNT } from "@/constants";
 const NKN_CONFIG = {
   worker: true,
   numSubClients: NKN_SUB_CLIENT_COUNT,
-  tls: false, // force https
+  tls: true, // force https
   reconnectIntervalMin: 500, // 300ms 失败自动重连
   // msgHoldingSeconds: 3, // 等3秒, 等rpc 重莲?
   sessionConfig: { mtu: MAX_MTU, flushInterval: 10 }, // 最大传输单元 20kb
